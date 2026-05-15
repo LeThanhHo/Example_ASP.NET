21fcf7fc75843bcfe14f33d24185fafb9257defa
Admin <Admin@D21308>
2026-05-15 15:03:23 +0700

    Add project files.

---
+++ CMS.Data/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿/*
+ Ten: Le Thanh Ho
+ MSSV: 2123110125
+Lop: 2123110125
+ 
+ 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Role { get; set; } // Quản trị viên hoặc Biên tập viên
+
+    }
+}
